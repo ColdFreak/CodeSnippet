8156ac8246ecff4321daeea02b542526168f84de
ColdFreak <madfrogme@gmail.com>
2012-11-15 23:31:28 +0900

    added

---
+++ name_to_address.c
@@ -0,0 +1,16 @@
+#include <netdb.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+
+int main(int argc, char **argv) {
+	int i;
+	struct hostent *host;
+	struct in_addr **list;
+	host = gethostbyname(argv[1]);
+	/* 把hostent中的h_addr_list 强制转换为(struct in_addr **)
+	 * 再用inet_ntoa(struct in_addr in)很重要  */
+	list = (struct in_addr **)host->h_addr_list;
+	for( i = 0; list[i]; i++)
+		printf("%s\n",inet_ntoa(*list[i] ));
+	return 0;
+}
