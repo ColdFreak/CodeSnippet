a183d382b48d7b45174a15383e58b24ace113a14
madfrogme@gmail.com <root@bt.foo.org>
2012-12-08 21:35:58 +0900

    add

---
+++ greeting.py
@@ -0,0 +1,7 @@
+# asks two people for their names
+#!/usr/bin/python
+
+name1 = raw_input("What is the first name? ")
+name2 = raw_input("What is the second name? ")
+
+print "Hello, " + name1+ " and " + name2 + "! how are you?"
