89dfbe511a580dd3accf3a8e7ac28b791f4b8d9f
Wang Zhijun <madfrogme@gmail.com>
2013-06-02 20:50:46 +0900

    display two messages in two threads

---
+++ pthread1.c
@@ -0,0 +1,67 @@
+/* gcc -lpthread pthread1.c */
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <pthread.h>
+ 
+
+void *print_message_function( void *ptr );
+
+ 
+int main() {
+
+     pthread_t thread1, thread2;
+
+     char *message1 = "Thread 1";
+
+     char *message2 = "Thread 2";
+
+     int  iret1, iret2;
+
+ 
+
+    /* Create independent threads each of which will execute function */
+
+ 
+	 // thread1 and thread2 is of type pthread_t
+     iret1 = pthread_create( &thread1, NULL, print_message_function, (void*) message1);
+
+     iret2 = pthread_create( &thread2, NULL, print_message_function, (void*) message2);
+
+ 
+
+     /* Wait till threads are complete before main continues. Unless we  */
+
+     /* wait we run the risk of executing an exit which will terminate   */
+
+     /* the process and all threads before the threads have completed.   */
+
+     pthread_join( thread1, NULL);
+
+	 // int pthread_join(pthread_t th, void **thread_return
+	 // if thread_return is not NULL, the return value of th
+	 // is stored in the location pointed to by thread_return
+     pthread_join( thread2, NULL);
+
+ 
+
+     printf("Thread 1 returns: %d\n",iret1);
+
+     printf("Thread 2 returns: %d\n",iret2);
+
+     exit(0);
+
+}
+
+ 
+
+void *print_message_function( void *ptr ) {
+
+     char *message;
+
+     message = (char *) ptr;
+
+     printf("%s \n", message);
+
+}
